ec7067afcb0224710b5a0a4ae08589fa19ebeb42
at2021 <>
2021-05-30 21:43:43 +0000

    I add simple scatter plot using random

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -19,6 +19,8 @@ import numpy as np
 import pandas as pd
 
 # YOUR CODE HERE 1 to set data
+data = pd.DataFrame(x= np.random.rand(1000),y=np.random.rand(1000))
+
 
 # Check your random values
 print(data.head())
@@ -31,7 +33,7 @@ assert len(data) == 1000, "There should be 1000 rows of data."
 # 
 
 # YOUR CODE HERE 2 to set colors
-
+colors = np.random.rand(1000)
 # This test print should print out 10 first numbers in the variable colors
 print(colors[0:10])
 
@@ -42,25 +44,27 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 # ### Part 3 
 # 
 # #### Part 3.1
-# 
+
 # Create a scatter plot of points with random colors
 # 
 # #### Part 3.2
-# 
+
 # #### Part 3.3
 # 
 
 # Plot a scatter plot
 # YOUR CODE HERE 3
-
+data.plot.scatter(s=50,c=np.random.rand(1000),colormap='rainbow',edgecolor='black')
 # Add labels and title
 # YOUR CODE HERE 4
-
+data.set_xlabel("X-label")
+data.set_ylabel("Y-label")
+data.set_title("My random candy points")
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
-
+data.savefig('my_first_plot.png')
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
 
